44613d4bf92e3b64dbf5e7c97bf71bca8f2cc559
Rishabh Rana <rishabh.r@hotmail.com>
2018-09-06 16:51:56 -0400

    Added a room bean

---
+++ src/main/java/com/revature/beans/Room.java
@@ -0,0 +1,40 @@
+package com.revature.beans;
+
+import javax.persistence.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROOM")
+public class Room implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4349272843711618060L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roomSequence")
+	@SequenceGenerator(allocationSize = 1, name = "roomSequence", sequenceName = "SQ_ROOM_PQ")
+	@Column(name = "ROOM_ID")
+	private int id;
+	
+	@Column(name = "MAX_PLAYERS")
+	private int maxPlayers;
+	
+	
+}
