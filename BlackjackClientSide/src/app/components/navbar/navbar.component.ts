82aa47e64790c90cc7b0b7d444c9a63b19ca89d7
Alan Sato <sam.alan.sato@gmail.com>
2018-09-12 09:26:19 -0400

    small changes

--- BlackjackClientSide/src/app/components/navbar/navbar.component.ts
+++ BlackjackClientSide/src/app/components/navbar/navbar.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  title = "Blackjack Online";
+
   isLoggedIn: Observable<boolean>;
 
 
