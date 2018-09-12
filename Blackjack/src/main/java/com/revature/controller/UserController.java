c6094dab06b2cae1800f790e9b1cdf0ad784dcea
iramin1234 <iramin1234@gmail.com>
2018-09-12 09:54:29 -0400

    user register, return response code

--- Blackjack/src/main/java/com/revature/controller/UserController.java
+++ Blackjack/src/main/java/com/revature/controller/UserController.java
@@ -1,20 +1,16 @@
 package com.revature.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.revature.beans.Account;
 import com.revature.beans.User;
@@ -37,14 +33,23 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	@ResponseStatus(HttpStatus.OK)
-	public String handleAccountFormRequest(@RequestBody MultiValueMap<String, String> formParams) {
+	@ResponseBody
+	public ResponseEntity<String> handleAccountFormRequest(@RequestBody MultiValueMap<String, String> formParams) {
+		ResponseEntity<String> resp = null;
 		System.out.println("form params received " + formParams);
-		Account a = new Account(formParams.getFirst("username"), formParams.getFirst("password"));
-		int startingBalance = 1000;
-		User u = new User(formParams.getFirst("firstname") , formParams.getFirst("lastname"), startingBalance, a);
-		userService.addUser(u);
-		return "forward:/dashboard.html";
+		
+		try {
+			Account a = new Account(formParams.getFirst("username"), formParams.getFirst("password"));
+			int startingBalance = 1000;
+			User u = new User(formParams.getFirst("firstname") , formParams.getFirst("lastname"), startingBalance, a);
+			userService.addUser(u);
+			
+			resp = new ResponseEntity<>("ACCOUNT CREATED SUCCESSFULLY", HttpStatus.OK);
+		} catch (Exception e) {
+			resp = new ResponseEntity<>("FAILED TO CREATE ACCOUNT", HttpStatus.BAD_REQUEST);
+			e.printStackTrace();
+		}
+		return resp;
 
 	}
 	
